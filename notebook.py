6aa4aed40797b84e38b060a6c6cb49f16696ff11
Uzair017 <uzairrafiq2627@gmail.com>
2026-09-24 23:19:09 +0200

    feat:Added the signature of fibonacci function

--- notebook.py
+++ notebook.py
@@ -9,5 +9,33 @@ def fibonacci(n):
     pass
 
 
+@app.cell
+def _():
+    # def test_fibonacci(n):
+    #     assert fibonacci(0) == 0
+    #     assert fibonacci(1) == 1
+    #     assert fibonacci(2) == 2
+    #     assert fibonacci(3) == 3
+    #     assert fibonacci(4) == 4
+    #     assert fibonacci(5) == 5
+    return
+
+
+@app.cell
+def _():
+    # def test_fibonacci_zero():
+    #     assert fibonacci(0) == 0
+
+    # def test_fibonacci_one():
+    #     assert fibonacci(1) == 1
+
+    # def test_fibonacci_sequence():
+    #     assert fibonacci(2) == 1
+    #     assert fibonacci(3) == 2
+    #     assert fibonacci(4) == 3
+    #     assert fibonacci(5) == 5
+    return
+
+
 if __name__ == "__main__":
     app.run()
