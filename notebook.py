e123fde66c29a0615e6d86a8e0360e6fb12149fb
Uzair017 <uzairrafiq2627@gmail.com>
2026-09-24 23:35:41 +0200

    feat:passed, providing implementation for fibonacci function

--- notebook.py
+++ notebook.py
@@ -6,16 +6,20 @@ app = marimo.App(width="medium")
 
 @app.function
 def fibonacci(n):
-    pass
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibonacci(n-1) + fibonacci(n-2)
 
 
 @app.function
 def test_fibonacci():
     assert fibonacci(0) == 0
     assert fibonacci(1) == 1
-    assert fibonacci(2) == 2
-    assert fibonacci(3) == 3
-    assert fibonacci(4) == 4
+    assert fibonacci(2) == 1
+    assert fibonacci(3) == 2
+    assert fibonacci(4) == 3
     assert fibonacci(5) == 5
 
 
