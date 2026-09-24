ef5cd7e12982157c2c493b4413e6b28efed369f1
Uzair017 <uzairrafiq2627@gmail.com>
2026-09-25 00:22:46 +0200

    feat:modified to optimized fibonacci function which handles large numbers

--- notebook.py
+++ notebook.py
@@ -6,8 +6,6 @@ app = marimo.App(width="medium")
 
 @app.cell
 def _(mo):
-
-
     mo.md("""
     #fibonacci tdd kata
     here I have implemented fibonacci seq using tdd.
@@ -49,14 +47,34 @@ def _(mo):
     return (user_input,)
 
 
+@app.cell
+def fibonacci():
+    #refactored version
+    # def fibonacci(n):
+    #      a=0
+    #      b=1
+    #      for _ in range(n):
+    #          a,b=b, a+b
+    #      return a
+    return
+
+
 @app.function
-#reactored version
+#latest optimized version which also handles large values
 def fibonacci(n):
-     a=0
-     b=1
-     for _ in range(n):
-         a,b=b, a+b
-     return a
+    if n<0:
+        raise ValueError("n must not be negative")
+    def fast_doubling_algo(k):
+        if k ==0:
+            return 0,1
+        a,b=fast_doubling_algo(k//2)
+        c= a*(2*b-a)
+        d=a*a+b*b
+
+        if k%2==0:
+            return c,d
+        return d,c+d
+    return fast_doubling_algo(n)[0]
 
 
 @app.cell
@@ -69,6 +87,12 @@ def _(mo, user_input):
 
 
 @app.function
+def test_10M_number_fibonacci():
+    result= fibonacci(10_000_000)
+    assert result>0
+
+
+@app.function
 def test_fibonacci():
     assert fibonacci(0) == 0
     assert fibonacci(1) == 1
@@ -76,6 +100,13 @@ def test_fibonacci():
     assert fibonacci(3) == 2
     assert fibonacci(4) == 3
     assert fibonacci(5) == 5
+
+
+@app.function
+#additional test for big values
+def test_largenumbers_fibonacci():
+    assert fibonacci(10) == 55
+    assert fibonacci(100) == 354224848179261915075
 
 
 if __name__ == "__main__":
