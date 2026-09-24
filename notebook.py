6823bd7e0e745279175589024a4a41d68d1adede
Uzair017 <uzairrafiq2627@gmail.com>
2026-09-25 00:00:04 +0200

    feat:added notebook document, marimo widget and displayed the result

--- notebook.py
+++ notebook.py
@@ -5,6 +5,27 @@ app = marimo.App(width="medium")
 
 
 @app.cell
+def _(mo):
+
+
+    mo.md("""
+    #fibonacci tdd kata
+    here I have implemented fibonacci seq using tdd.
+
+    the process is as:
+    green-> red ->refactor
+    """)
+    return
+
+
+@app.cell
+def _():
+    import marimo as mo
+
+    return (mo,)
+
+
+@app.cell
 def _():
     #version1
     # def fibonacci(n):
@@ -14,6 +35,18 @@ def _():
     #         return 1
     #     return fibonacci(n-1) + fibonacci(n-2)
     return
+
+
+@app.cell
+def _(mo):
+    user_input=mo.ui.number(
+        start=0,
+        step=1,
+        value=10,
+        label="n"
+    )
+    user_input
+    return (user_input,)
 
 
 @app.function
@@ -26,6 +59,15 @@ def fibonacci(n):
      return a
 
 
+@app.cell
+def _(mo, user_input):
+    result=fibonacci(user_input.value)
+    mo.md(
+        f"**F({user_input.value}) = {result}**"
+    )
+    return
+
+
 @app.function
 def test_fibonacci():
     assert fibonacci(0) == 0
@@ -34,11 +76,6 @@ def test_fibonacci():
     assert fibonacci(3) == 2
     assert fibonacci(4) == 3
     assert fibonacci(5) == 5
-
-
-@app.cell
-def _():
-    return
 
 
 if __name__ == "__main__":
